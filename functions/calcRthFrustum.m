2a2aeaa7aaccba4ec543ba30dfd3a5291599c812
Michael Meiler <michael-meiler@gmx.de>
2022-09-26 10:01:50 +0200

    Delete calcRthFrustum.m

--- functions/calcRthFrustum.m
+++
@@ -1,8 +0,0 @@
-function [thermalRes] = calcRthFrustum(lambda,x,h,beta)
-
-    % Calculate thermal resistance of a square pyramid frustum.
-    % h = height.
-    % x = length of top square area.
-    thermalRes = 1/lambda * h/(2*h*x*tan(beta)+x^2);
-
-end
