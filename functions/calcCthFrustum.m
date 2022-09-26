8157e002d43ba24dd7178d0a3de908fe158e9dbe
Michael Meiler <michael-meiler@gmx.de>
2022-09-26 10:01:43 +0200

    Delete calcCthFrustum.m

--- functions/calcCthFrustum.m
+++
@@ -1,9 +0,0 @@
-function [thermalCap] = calcCthFrustum(roh,cp,x,h,angle)
-
-    % Calculate thermal capacity of a square pyramid frustum.
-    % x = length of top square area.
-    % h  = height.
-    % angle = angle between h and pyramids sides.
-    thermalCap = roh*cp*h* ( x^2 + 2*h*x*tan(angle) + 4/3*h^2*tan(angle));
-
-end
